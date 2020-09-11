7e1c7f4ab2417757ece56e46a2731f0851b444f3
Annalise Murphy <nali247@gmail.com>
2020-09-11 11:38:49 -0700

    addOrder function added and passing tests

--- pizza-delivery/lib/schedule.js
+++ pizza-delivery/lib/schedule.js
@@ -2,9 +2,24 @@ class Schedule {
     MINUTES_PER_DAY = 60 * 24;
     #schedule = Array(this.MINUTES_PER_DAY).fill(false);
 
+    // nextOpenMinute
     nextOpenMinute() {
         return this.#schedule.findIndex(minute => !minute);
     }
+
+    // freeWindow
+    freeWindow() {
+        return this.#schedule
+            .slice(startMinute, endMinute)
+            .every(minute => !minute);
+    }
+
+    addOrder(order) {
+        for(let i = order.startMinute; i <= order.deliveryMinute; i++) {
+            this.#schedule[i] = true;
+        }
+    }
+
 
 }
 
--- pizza-delivery/lib/schedule.test.js
+++ pizza-delivery/lib/schedule.test.js
@@ -1,22 +1,29 @@
 const Schedule = require('./schedule');
+const Order = require('./order');
 
 
-// nextOpenMinute 
 describe('Schedule Class', () => {
     it('finds the next unscheduled minute', () => {
         const schedule = new Schedule();
 
         expect(schedule.nextOpenMinute()).toEqual(0);
     });
+
+    
+    it('adds a new order to the schedule', () => {
+        const schedule = new Schedule();
+        const order = new Order('pizza order', 40, 20);
+        schedule.addOrder(order);
+
+        expect(schedule.nextOpenMinute()).toEqual(41);
+    });
+    
+    // clear
+    
+    
+    
+    
+    
 })
-
-// freeWindow
-
-
-// clear
-
-
 // print
 
-
-// addOrder
